429a4e543083c49f10247ad7603394be8c52f21b
JamieC2002 <jamiech2002@gmail.com>
2022-08-29 16:11:58 -0400

    set input's class to titlebox, and make the text center-aligned

--- components/TitleBox.js
+++ components/TitleBox.js
@@ -8,13 +8,10 @@ import styles from '../styles/TitleBox.module.css';
  */
 const TitleBox = () => {
   return (
-    <div className={styles.titlebox}>
-      <div className="form-floating">
-        <input className="form-control form-control-lg" type="text" 
-               placeholder=".form-control-lg" 
-               aria-label=".form-control-lg example"
-        />
-      </div>
+    <div>
+      <input className = {styles.titlebox} type="text" 
+             name="Event-Title" placeholder="Enter event title" 
+      />
     </div>
   )
 }
