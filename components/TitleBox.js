cf6ed485075d0bc813b725365f6ec7531c8928a7
JamieC2002 <jamiech2002@gmail.com>
2022-10-16 23:40:09 -0400

    Modularize the Title Box and added Submission button (should we put the submission button in another part?)

--- components/TitleBox.js
+++ components/TitleBox.js
@@ -5,22 +5,56 @@ import styles from '../styles/TitleBox.module.css';
 import { useState } from 'react'
 
 /**
+ * @Component Create Event Button
+ */
+const Button = (props) => {
+  return ( 
+    <input type="button" name="Create Event Button" value="Create Event" onClick={props.onClick} className={styles.button}/>
+    )
+} 
+
+/**
  * @Component Event Name title field
  */
-const TitleBox = () => {
+const TitleBox = (props) => {
+  return (
+    <form Name="Event Title" Action="" Method="GET">
+      <input type="text" name="Event-Title" onChange={props.onChange} className={styles.titlebox}/>
+    </form>
+  )
+}
+/**
+ * @Component Main Function For TitleBox
+ */
+const renderTitleBox = () => {
   const [eventTitle] = useState(0)
 
+  // Keeps track of the Event Title
   const listenEventTitle = (e) => {
     eventTitle = e.target.value;
+
+    // Debugging: check eventTitle
+    console.log(eventTitle)
+  }
+
+  // Creates Event -> 
+  const handleCreateEvent = () => {
+    if (eventTitle === "") {
+      alert("Please enter Event name!");
+    } 
+    console.log(eventTitle);
   }
 
   return (
     <div>
-      <form Name="Event Title" Action="" Method="GET">
-        <input type="text" name="Event-Title" onChange={listenEventTitle} className={styles.titlebox}/>
-      </form>
+      <TitleBox 
+        onChange={listenEventTitle}
+      />
+      <Button 
+        onClick={handleCreateEvent}
+      />
     </div>
   )
 }
 
-export default TitleBox;
+export default renderTitleBox;
