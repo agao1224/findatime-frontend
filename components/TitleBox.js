11fa4c3eb0dda0ac7b803b45402071687e971eae
JamieC2002 <jamiech2002@gmail.com>
2022-10-16 20:38:48 -0400

    Added an initial state that keeps track of the event title name

--- components/TitleBox.js
+++ components/TitleBox.js
@@ -2,17 +2,23 @@
  * @Imports
  */
 import styles from '../styles/TitleBox.module.css';
+import { useState } from 'react'
 
 /**
  * @Component Event Name title field
  */
 const TitleBox = () => {
+  const [eventTitle] = useState(0)
+
+  const listenEventTitle = (e) => {
+    eventTitle = e.target.value;
+  }
+
   return (
     <div>
-      <input className={styles.titlebox} 
-             name="Event-Title" 
-             type="text" 
-      />
+      <form Name="Event Title" Action="" Method="GET">
+        <input type="text" name="Event-Title" onChange={listenEventTitle} className={styles.titlebox}/>
+      </form>
     </div>
   )
 }
