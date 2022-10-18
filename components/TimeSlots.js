f8a2d54ea8ca47a51aec3833ea6e5a60c3a3b479
JamieC2002 <jamiech2002@gmail.com>
2022-10-17 23:15:37 -0400

    Added Time Availability Page, and component for time slots. Will plan out before implementation

---
+++ components/TimeSlots.js
@@ -0,0 +1,18 @@
+/**
+ * @Imports
+ */
+ import styles from '../styles/TimeSlots.module.css';
+
+ // PLAN FIRST:
+ // Create event listeners for hovering mouse on/off
+ // Need to make the times and boxes for them
+ const TimeSlot = () => {
+    return (
+        <div>
+            Group Availability
+        </div>
+    )
+ }
+
+ export default TimeSlot;
+
