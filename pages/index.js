884401fc6c72fb756f63f91acfabe2683528dfba
agao-howetech <agao@howe-technology.com>
2022-12-15 16:30:04 -0500

    added create event functionality using fetch API, to POST /events

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
  * @Imports
  */
 import Head from 'next/head';
-import TitleBox from '../components/TitleBox';
 import DaySelector from '../components/DaySelector';
 import TimeSelector from '../components/TimeSelector';
 import styles from '../styles/Home.module.css';
@@ -13,8 +12,54 @@ import { useState } from 'react';
  * @Page Root page 
  */
 const Home = () => {
+  const [days, setDays] = useState([]);
+  const [eventTitle, setEventTitle] = useState("");
   const [startTime, setStartTime] = useState("Start Time");
   const [endTime, setEndTime] = useState("End Time");
+
+  const convertStrToTime = (time) => {
+    // Look at last two indices, AM or PM 
+    const strLen = time.length;
+    const amOrPM = time.charAt(strLen-2) + "" + time.charAt(strLen-1);
+
+    let militaryTime = 0;
+    if (amOrPM === "PM") {
+      militaryTime += 1200 
+    }
+
+    let offset = 0;
+    // Look at first two indices, check if the second is ":" or number
+    if (time.charAt(1) === ":") {
+      offset = (parseInt(time.charAt(0)))
+    } else {
+      let tmp = parseInt((time.charAt(0) + "" + time.charAt(1)));
+    }
+    offset = offset * 100;
+    return (militaryTime + offset);
+  }
+
+  const createEvent = async () => {
+    const eventObj = {
+      name: eventTitle,
+      days: days,
+      startTime: convertStrToTime(startTime),
+      endTime: convertStrToTime(endTime)
+    }
+    
+    console.log(eventObj);
+
+    const createEventResponse = await fetch("http://localhost:5000/events", {
+      method: "POST",
+      body: JSON.stringify(eventObj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    const createEventData = await createEventResponse.json();
+    console.log(createEventData);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,12 +68,21 @@ const Home = () => {
 
       <main className={styles.main}>
         <h1> Event Title </h1>
-        <TitleBox />
+        <div className={styles.titlebox}>
+          <div className="form-floating">
+            <input className="form-control form-control-lg" type="text" 
+                  placeholder=".form-control-lg" 
+                  aria-label=".form-control-lg example"
+                  value={eventTitle}
+                  onChange={(event) => setEventTitle(event.target.value)}
+            />
+          </div>
+        </div>
       </main>
 
       <div className={styles.body}>
         <div className={styles.dayselector}>
-          <DaySelector />
+          <DaySelector days={days} setDays={setDays}/>
         </div>
         <div className={styles.timeselector}>
           <TimeSelector timeBound={startTime} setTimeBound={setStartTime} 
@@ -39,7 +93,10 @@ const Home = () => {
       </div>
       
       <div className={styles.createbutton}>
-        <button type="button" className="btn btn-success btn-lg">Create Event!</button>
+        <button type="button" className="btn btn-success btn-lg"
+         onClick={createEvent}>
+          Create Event!
+        </button>
       </div>
 
       <footer className={styles.footer}>
