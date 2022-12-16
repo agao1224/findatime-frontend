2c72407226c358fe4369b747ac32a69f63fd6e5b
agao2cmu <agao2@andrew.cmu.edu>
2022-12-16 03:39:30 -0500

    created dynamic route to display event info

---
+++ pages/[eventID].js
@@ -0,0 +1,18 @@
+/**
+ * @Imports 
+ */
+import styles from '../styles/EventPage.module.css'
+
+/**
+ * @Component Page to render event information given specific
+ *            event ID (uri)
+ */
+const EventPage = () => {
+  return (
+    <div>
+      <h1> hello </h1>
+    </div>
+  )
+}
+
+export default EventPage;
