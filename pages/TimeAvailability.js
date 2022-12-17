329d06594459b628d1e03499ec75e8468e62e543
JamieC2002 <jamiech2002@gmail.com>
2022-12-16 21:14:16 -0500

    just commit, but will sync to current version

--- pages/TimeAvailability.js
+++ pages/TimeAvailability.js
@@ -1,13 +1,16 @@
 /**
  * @Imports
  */
-
+import TimeSlots from '../components/TimeSlots';
 /**
  * @Page Time Availability Page
  */
 const TimeAvailability = () => {
     return (
         <div>
+           <TimeSlots />
         </div>
     )
 }
+
+export default TimeAvailability;
