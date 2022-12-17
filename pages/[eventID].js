24ff49de6a96099e5dae1bf36ea7a027ce527b15
agao2cmu <agao2@andrew.cmu.edu>
2022-12-16 19:05:55 -0500

    got dynamic route rendering to work for event pages

--- pages/[eventID].js
+++ pages/[eventID].js
@@ -3,16 +3,41 @@
  */
 import styles from '../styles/EventPage.module.css'
 
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+
 /**
  * @Component Page to render event information given specific
- *            event ID (uri)
+ *        event ID (uri)
  */
 const EventPage = () => {
-  return (
-    <div>
-      <h1> hello </h1>
-    </div>
-  )
+  const [eventInfo, setEventInfo] = useState({});
+  const { query, isReady } = useRouter();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!isReady) {
+      setLoaded(false);
+    } else {
+      const eventID = query.eventID;
+
+      fetch(`http://localhost:5000/events/${eventID}`)
+        .then(res => res.json())
+        .then(data => setEventInfo(data));   
+
+      setLoaded(true);
+    }
+  }, [isReady]);
+
+  if (loaded) {
+    return (
+      <div className={styles.eventmain}>
+        <h1> {eventInfo.name} </h1>
+      </div>
+    )
+  } else {
+    return <div> Loading... </div>
+  }
 }
 
 export default EventPage;
